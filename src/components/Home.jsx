d2b0f851b9a15865af4c50b892879a52a6f5938e
Sean O'Daniels <sodaniels@gmail.com>
2024-03-11 12:33:54 -0400

    Fixed display error on homepage if user is invalid.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,21 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   const user = useSelector(state => state.user)
+  if (user) {
   return (
     <div id="home">
       <h2>Home</h2>
       <p>Welcome, {user.username}. Check out <Link to="/lists">your lists</Link>.</p>
     </div>
   )
+  } else {
+    return (
+      <div id="home">
+        <h2>Home</h2>
+        <p>Welcome. Please <Link to="/my-account">log in to continue</Link>.</p>
+      </div>
+    )
+  }
 }
 
 export default Home
