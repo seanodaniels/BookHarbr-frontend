2138833f9d9960cbd5f3b32ce8e11f2c8dee2bac
Sean O'Daniels <sodaniels@gmail.com>
2024-03-14 18:15:17 -0400

    WIP: book detail page.

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -1,32 +1,49 @@
 import { useNavigate, useMatch, useSearchParams, Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import olService from '../services/ol'
 import { useDispatch, useSelector } from 'react-redux'
 import { createNotification, createError } from '../reducers/alertReducer'
 
 const BookDetail = () => {
-  const [ searchType, setSearchType ] = useState(null)
-  const [ searchKey, setSearchKey ] = useState(null)
   const [ workInfo, setWorkInfo ] = useState(null)
+  const [ authorInfo, setAuthorInfo ] = useState([])
+  const hasPageBeenRendered = useRef(false)
 
   const dispatch = useDispatch()
 
   const match = useMatch('/works/:key')
-  
+
   useEffect(() => {
-    try {
-      olService
-      .olWorksSearch(match.params.key)
-      .then(w => {
-        if (w) {
-          setWorkInfo(w)
-        } else {
-          dispatch(createNotification('No results found.', 10000))
-        }
-      })
-    } catch (error) {
-        dispatch(createNotification(`Error: ${error}`))
+
+    const fetchWorks = async () => {
+      try {
+        const results = olService.olWorksSearch(match.params.key)
+        return results
+      } catch (error) {
+        null
+      }
     }
+
+    const fetchAuthors = async (authorKey) => {
+      try {
+        const results = olService.olAuthorSearch(workInfo.authors[0].author.key)
+        return results
+      } catch (error) {
+        null
+      }
+    }
+
+    const fetchAll = async () => {
+      const getWorks = await fetchWorks()
+      console.log('1st author', getWorks.authors[0].author.key)
+      const getAuthors = await fetchAuthors(getWorks.authors[0].author.key)
+      console.log('getAuthors', getAuthors)
+      setWorkInfo(getWorks)
+      setAuthorInfo(getAuthors)
+    }
+
+    fetchAll()
+
   }, [])
 
   // Prevent re-rendering if the user refreshes the browser
@@ -34,13 +51,20 @@ const BookDetail = () => {
     return null
   }
 
+  const imageUrl = `https://covers.openlibrary.org/b/id/${workInfo.covers[0]}.jpg`
+  // https://covers.openlibrary.org/b/id/works/OL20128158W.jpg
+
   return (
     <div id="book-detail">
-      <h2>Book Detail</h2>
-      <pre>
+
+      <hr /><h2>WORK</h2><pre>
       {
-        JSON.stringify(workInfo, null, 2)   
-      
+        JSON.stringify(workInfo, null, 2)
+      }
+      </pre>
+      <hr /><h2>AUTHOR</h2><pre>
+      {
+        JSON.stringify(authorInfo, null, 2)
       }
       </pre>
     </div>
