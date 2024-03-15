3d7fda89de0776e40fe28f8c804efb5d7549fe75
Sean O'Daniels <sodaniels@gmail.com>
2024-03-15 11:17:40 -0400

    feat: BookDetail elements placed

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -7,7 +7,7 @@ import { createNotification, createError } from '../reducers/alertReducer'
 const BookDetail = () => {
   const [ workInfo, setWorkInfo ] = useState(null)
   const [ authorInfo, setAuthorInfo ] = useState([])
-  const hasPageBeenRendered = useRef(false)
+  
   const MAX_NUMBER_OF_AUTHORS_DISPLAYED = 4
 
   const dispatch = useDispatch()
@@ -35,9 +35,10 @@ const BookDetail = () => {
     }
 
     const fetchAll = async () => {
+      // Get Work info
       const getWorks = await fetchWork()
-      console.log('1st author', getWorks.authors[0].author.key)
 
+      // Get Author(s) but fetch a maximum equal to MAX_NUMBER_OF_AUTHORS_DISPLAYED
       let authorArray = []
       const numAuthors = getWorks.authors.length <= MAX_NUMBER_OF_AUTHORS_DISPLAYED 
         ? getWorks.authors.length 
@@ -45,36 +46,73 @@ const BookDetail = () => {
       for (let i = 0; i < numAuthors; i++) {
         authorArray[i] = await fetchAuthor(getWorks.authors[i].author.key)
       }
+
+      // Set page state
       setWorkInfo(getWorks)
       setAuthorInfo(authorArray)
     }
 
+    // Get all page information
     fetchAll()
 
   }, [])
+
+  const buildCoverImageUrl = () => {
+    return workInfo.covers 
+    ? `https://covers.openlibrary.org/b/id/${workInfo.covers[0]}.jpg`
+    : '/images/Image_not_available.png'
+  }
+
+  const buildAuthorList = () => {
+    const authorList = authorInfo.map(a => a.name)
+    return authorList.toString().replace(/,/g, ', ')
+  }
+
+  const buildDescription = () => {
+    let rawDescription = ''
+    workInfo.description
+    ? workInfo.description.value
+      ? rawDescription = workInfo.description.value
+      : rawDescription = workInfo.description
+    : null
+    return rawDescription
+  }
+
+  const debugInfo = () => {
+    return (
+      <div>
+        <hr />
+        <h2>WORK</h2><pre>
+        {
+          JSON.stringify(workInfo, null, 2)
+        }
+        </pre>
+        <hr />
+        <h2>AUTHOR</h2><pre>
+        {
+          authorInfo.map(a => <p key={a.key}>{a.name}</p>)
+        }
+        </pre>
+      </div>
+    )
+  }
 
   // Prevent re-rendering if the user refreshes the browser
   if (!workInfo) {
     return null
   }
 
-  if (workInfo.covers) {
-    const imageUrl = `https://covers.openlibrary.org/b/id/${workInfo.covers[0]}.jpg`
-  }
-
   return (
     <div id="book-detail">
+      { workInfo.title ? <h2>{workInfo.title}</h2> : null }
+      { authorInfo.length > 0
+        ? <div className="author">by {buildAuthorList()}</div>
+        : null
+      }
+      { workInfo.covers ? <img className="book-cover" src={buildCoverImageUrl()} /> : null }
+      { workInfo.description ? <div className="description">{buildDescription()}</div> : null }
 
-      <hr /><h2>WORK</h2><pre>
-      {
-        JSON.stringify(workInfo, null, 2)
-      }
-      </pre>
-      <hr /><h2>AUTHOR</h2><pre>
-      {
-        authorInfo.map(a => <p key={a.key}>{a.name}</p>)
-      }
-      </pre>
+      {/* { debugInfo() } */}
     </div>
   )
 }
